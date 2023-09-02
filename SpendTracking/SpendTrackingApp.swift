1b8f61c01dc05a1011b5e2544583334445f48eae
Yifan Wang <8groupoid@gmail.com>
2023-09-02 08:58:35 -0400

    Initial Commit

---
+++ SpendTracking/SpendTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendTrackingApp.swift
+//  SpendTracking
+//
+//  Created by Alex Wang on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SpendTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
