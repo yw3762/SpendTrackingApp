4aac2f1eb84bbb610c9ee47fddb856908d5c4997
Yifan Wang <8groupoid@gmail.com>
2023-10-26 11:19:20 -0400

    index on dashboard: ec8d427 Finished formatting Recurring charges BlockView

---
+++ SpendTracking/Views/AccountView/AccountDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountDetailsView.swift
+//  SpendTracking
+//
+//  Created by Alex Wang on 10/25/23.
+//
+
+import SwiftUI
+
+struct AccountDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountDetailsView()
+}
